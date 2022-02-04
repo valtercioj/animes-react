6ab0dcb7cb394d2a35345fe01859372a53e3b428
valtercio santiago <juniorvaltercio2000@gmail.com>
2022-02-04 02:17:29 -0300

    adicionando debounce

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ export default function App() {
 
     const [text, setText] = useState('')
     const [info, setInfo] = useState({})
+
     const api = 'https://kitsu.io/api/edge/'
 
     useEffect(() => {
+        setInfo({})
         if (text) {
+
             fetch(`${api}anime?filter[text]=${text}&page[limit]=12
         `)
                 .then((response) => response.json())
                 .then((response) => setInfo(response))
-        }else{
-            setInfo({})
         }
     }, [text]);
+
     return (
         <div className="App">
             <h1>Animes</h1>
@@ -25,15 +27,19 @@ export default function App() {
                 onChange={(search) => setText(search)}
             />
 
+            {text && !info.data && (
+                <span>Carregando...</span>
+            )}
+
             {info.data && (
                 <ul className="animes-list">
-                {info.data.map((anime) =>(
-                    <li key={anime.id}>
-                        <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/>
+                    {info.data.map((anime) => (
+                        <li key={anime.id}>
+                            <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle} />
 
-                        {anime.attributes.canonicalTitle}
-                    </li>
-                ))}
+                            {anime.attributes.canonicalTitle}
+                        </li>
+                    ))}
                 </ul>
             )}
         </div>
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
+import useDebounce from "../useDebounce";
 
-export default function SearchInput({value, onChange}){
-    function handleChange(event){
-        onChange(event.target.value)
+export default function SearchInput({ value, onChange }) {
+    const [displayValue, setDisplayValue] = useState(value)
+    const debounceChange = useDebounce(onChange, 500)
+
+    function handleChange(event) {
+        setDisplayValue(event.target.value)
+        debounceChange(event.target.value)
     }
-    return <input className="animes-input" type="search" value={value} onChange={handleChange}/>
+
+    return (
+        <input className="animes-input" type="search"
+            value={displayValue}
+            onChange={handleChange}
+        />
+
+    )
 }
